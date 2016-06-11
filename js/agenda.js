98f556cbe01f28698c63e6712d8c744663ad2a86
Simona Banut <banutika02@gmail.com>
2016-06-11 11:29:20 +0300

    date din array

--- js/agenda.js
+++ js/agenda.js
@@ -1,4 +1,20 @@
 console.debug('loading agenda');
 
-var row = '<tr><td>Nacom</td><td>Andrei</td><td>0723569874</td></tr>';
-$('#agenda tbody').append(row);
+function getRow(person){
+    var firstname=person[0];
+    var lastname=person[1];
+    var row = '<tr><td>'+firstname+'</td><td>'+ lastname +'</td><td>0723569874</td></tr>';
+    return row;
+}
+var contacts=[
+    ['Simion','Matei'],
+    ['Mocan','Nicu'],
+    ['Hader','Ramona']
+    //['popescu','elena'],
+    //['bighe','george']
+
+];
+for (var i=0; i<contacts.length; i++)
+    {
+        var person=contacts[i];
+        $('#agenda tbody').append(getRow(person));}
