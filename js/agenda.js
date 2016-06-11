c028787de64c6195b88d6a5907ac06961a19daf4
Simona Banut <banutika02@gmail.com>
2016-06-11 11:40:05 +0300

    date din json

--- js/agenda.js
+++ js/agenda.js
@@ -1,20 +1,20 @@
 console.debug('loading agenda');
 
-function getRow(person){
-    var firstname=person[0];
-    var lastname=person[1];
-    var row = '<tr><td>'+firstname+'</td><td>'+ lastname +'</td><td>0723569874</td></tr>';
+function getRow(person) {
+    var firstname = person.firstname;
+    var lastname = person.lastname;
+    var row = '<tr><td>' + firstname + '</td><td>' + lastname + '</td><td>0723569874</td></tr>';
     return row;
 }
-var contacts=[
-    ['Simion','Matei'],
-    ['Mocan','Nicu'],
-    ['Hader','Ramona']
+var contacts = [
+    {firstname:'SimionS',lastname: 'Matei'},
+    {firstname:'Mocan', lastname:'Nicu'},
+    {firstname:'Hader',lastname: 'Ramona'}
     //['popescu','elena'],
     //['bighe','george']
-
 ];
-for (var i=0; i<contacts.length; i++)
-    {
-        var person=contacts[i];
-        $('#agenda tbody').append(getRow(person));}
+
+for (var i = 0; i < contacts.length; i++) {
+    var person = contacts[i];
+    $('#agenda tbody').append(getRow(person));
+}
