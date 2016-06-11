70d938c553bc968b10c09a85730615ad60af7a45
Simona Banut <banutika02@gmail.com>
2016-06-11 13:25:41 +0300

    STERGERE UN CONTACT

--- js/agenda.js
+++ js/agenda.js
@@ -1,9 +1,12 @@
 console.debug('1) loading agenda');
 
 function getRow(person) {
-    var firstname = person.firstname;
-    var lastname = person.lastname;
-    var row = '<tr><td>' + firstname + '</td><td>' + lastname + '</td><td>0723569874</td></tr>';
+    var row = '<tr>'+
+        '<td>' + person.firstname + '</td>'+
+        '<td>' + person.lastname + '</td>'+
+        '<td>'+ person.telephone +'</td>'+
+        '<td><button data-id="' + person.id + '">x</button></td>' +
+        '</tr>';
     return row;
 }
 
@@ -14,10 +17,37 @@ $.ajax({
     console.debug ('3)ajax done', dateleDePeServer);
     showContacts(dateleDePeServer)
 });
+
+function removeContact (id){
+    $.ajax({
+        url: "js/mocks/remove-contact.json",
+        type: 'POST',
+        data: {
+            id: id
+        }
+
+    }).done(function(dateleDePeServer) {
+         showContacts(dateleDePeServer)
+    });
+
+}
 console.debug ('2) after ajax')
+
 function showContacts(contacts) {
+
+    //STERGE VECHILE DATE CA SA AFISEZE CELE NOI
+    $('#agenda tbody').html('');
+
+    //AFISEAZA NOILE DATE
     for (var i = 0; i < contacts.length; i++) {
         var person = contacts[i];
         $('#agenda tbody').append(getRow(person));
     }
 }
+
+//LA CLICK PE BUTTON FACE CEVA
+$('#agenda').on('click', 'button', function(){
+    var id=$(this).data('id');
+    console.info('remove this', this);
+    removeContact(id);
+})
